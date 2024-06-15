07241b70f05cc1fcf705b4f8a01dd905c15d6f9d
Catsim <111745304+catsums@users.noreply.github.com>
2024-06-15 16:11:55 +0200

    Made it ts instead

--- src/useSignalEvent.ts
+++ src/useSignalEvent.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { EventType, Handler, SignalEventBus } from "./SignalEvents";
 
 export const SignalEvent = {
@@ -70,25 +70,7 @@ export const SignalEvent = {
  * ```
  */
 export function useSignalEvent(event:EventType, handler:Handler, signalBus?:SignalEventBus) : void;
-/**
- * 
- * @param events 
- * A list of events that is meant to be subscribed to
- * @param handler
- * A listener that will run when the event is emitted.
- * @param signalBus
- * Optional SignalEventBus that will handle the signal emitting. If not specified, you can use the Global event bus.
- * 
- */
 export function useSignalEvent(events:EventType[], handler:Handler, signalBus?:SignalEventBus) : void;
-/**
- * 
- * @param handler
- * A listener that will run when the nameless event is emitted
- * @param signalBus
- * Optional SignalEventBus that will handle the signal emitting. If not specified, you can use the Global event bus.
- * 
- */
 export function useSignalEvent(handler:Handler, signalBus?:SignalEventBus) : void;
 export function useSignalEvent(...args:any[]){
 
@@ -139,13 +121,78 @@ function OffSignalEvent(event:EventType, handler:Handler, signalBus:SignalEventB
 	return SignalEvent;
 }
 
-export function SignalEventEvent({on="", listener, signalBus}:{
+/**
+ * This treats the signalbus as a component which can be placed anywhere in any other component to listen to events
+ * @param props.handler 
+ * A handler function to run when the event when emitted
+ * @param props.on 
+ * Event meant to be passed. It can be blank for a nameless event
+ * @param props.signalBus 
+ * Optional signalbus that will be used to emit the signals. Defaults to the global signal bus if no signal bus is passed.
+ * @returns 
+ * A component that can be placed in the application, which does not render anything on the DOM.
+ * 
+ * @example
+ * Here is how to trigger the count state of any component that has a listener to a count event
+ * 
+ * ```tsx
+ * 
+ * 	function onClick(){
+ * 		SignalEventBus.Global.emit("count");
+ * 	}
+ * 
+ * 	//button on click does not need to know about the component its in
+ * 	function Btn(){
+ * 		return (
+ * 			<button onClick={onClick}>
+ * 				Count
+ * 			</button>
+ * 		)
+ * 	}
+ * 
+ * 	function Comp(){
+ * 		let [count, setCount] = useState(0);
+ * 
+ * 		//only counts up by listening to "count" event, regardless of what triggers it
+ * 		function onCount(){
+ * 			setCount(count+1);
+ * 		}
+ * 
+ * 		return (
+ * 			<div>
+ * 				<SignalEventListener handler={onCount}/>
+ * 				<div>Count: {count}</div>
+ * 				<Btn/>
+ * 			</div>
+ * 		)
+ * 	}
+ * 	function Comp2(){
+ * 		let [count, setCount] = useState(0);
+ * 
+ * 		//this will be triggered too
+ * 		function onCount(){
+ * 			setCount(count+1);
+ * 		}
+ * 
+ * 		return (
+ * 			<div>
+ * 				<SignalEventListener handler={onCount}/>
+ * 				<div>Count: {count}</div>
+ * 			</div>
+ * 		)
+ * 	}
+ * 
+ * ```
+ */
+export function SignalEventListener({on="", handler, signalBus=SignalEventBus.Global}:{
 	on?: EventType,
-	listener: Handler,
+	handler: Handler,
 	signalBus?: SignalEventBus;
 }){
 
-	useSignalEvent(on, listener, signalBus);
+	useSignalEvent(on, handler, signalBus);
 
-	return (<></>)
+	return (
+		React.createElement(React.Fragment)
+	)
 }
